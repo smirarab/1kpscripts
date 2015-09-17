dfc5c063cce0f4806c249405cf92fb2401992078
Siavash Mirarab <smirarab@gmail.com>
2015-09-17 00:29:34 -0500

    update mask

--- mask-for-gt.sh
+++ mask-for-gt.sh
@@ -6,6 +6,8 @@ percent=$2
 taxapercent=$3
 out=$f.mask${percent}sites.mask${taxapercent}taxa.fasta
 
+test $# == 4 || { echo  USAGE: gene site_percent taxa_percent file_name; exit 1;  }
+
 m=`echo $( grep ">" $f|wc -l ) \* $percent / 100 |bc`
 run_seqtools.py -infile $f -masksites $m -outfile $f.mask${percent}sites.fasta
 echo `simplifyfasta.sh $f|wc -L` $f
